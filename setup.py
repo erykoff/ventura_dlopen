e28d3b919d8ba5f8f16b97e7481f1004fd811605
Eli Rykoff <erykoff@stanford.edu>
2023-01-07 21:25:22 -0800

    Add a bunch of libraries.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 # Available at setup time due to pyproject.toml
 # from pybind11 import get_cmake_dir
@@ -16,13 +17,50 @@ __version__ = "0.0.1"
 #   Sort input source files if you glob sources to ensure bit-for-bit
 #   reproducible builds (https://github.com/pybind/python_example/pull/53)
 
+
+
 ext_modules = [
     Pybind11Extension("ventura_dlopen",
-        ["ventura_dlopen/main.cpp"],
-        # Example: passing in the version to the compiled code
-        define_macros = [('VERSION_INFO', __version__)],
-        ),
+                      ["ventura_dlopen/main.cpp"],
+                      define_macros=[('VERSION_INFO', __version__)],
+                      library_dirs=os.environ['LSST_LIBRARY_PATH'].split(":"),
+                      libraries=[
+                          # conda libs
+                          "cfitsio",
+                          "gsl",
+                          "blas",
+                          "Minuit2",
+                          "log4cxx",
+                          "boost_math_c99",
+                          "ast",
+                          "ast_pal",
+                          "ast_cminpack",
+                          "ast_grf_2.0.9",
+                          "ast_grf_3.2.9",
+                          "ast_grf_5.6.9",
+                          "ast_grf3d",
+                          "ast_err",
+                          "yaml",
+                          "fftw3f",
+                          "fftw3",
+                          # lsst libs
+                          "meas_algorithms",
+                          "meas_base",
+                          "afw",
+                          "log",
+                          "daf_base",
+                          "geom",
+                          "sphgeom",
+                          "cpputils",
+                          "pex_exceptions",
+                          "base",
+                          "astshim",
+                      ]
+                      ),
 ]
+
+# Need to modify LDFLAGS to remove -Wl,-dead_strip_dylibs
+# Need library_dirs for our stuff.
 
 setup(
     name="ventura_dlopen",
